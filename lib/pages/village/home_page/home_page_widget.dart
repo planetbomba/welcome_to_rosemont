67a6b1057c7c5513e76e6b901be4ccbd9c4dc553
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-06 19:02:35 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/village/home_page/home_page_widget.dart
+++ lib/pages/village/home_page/home_page_widget.dart
@@ -208,7 +208,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                         options: FFButtonOptions(
                           height: 40.0,
                           padding: const EdgeInsetsDirectional.fromSTEB(
-                              40.0, 0.0, 40.0, 0.0),
+                              45.0, 0.0, 45.0, 0.0),
                           iconPadding: const EdgeInsetsDirectional.fromSTEB(
                               0.0, 0.0, 0.0, 0.0),
                           color: FlutterFlowTheme.of(context).primary,
